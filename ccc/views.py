433ca9ed75d9dd4e6d1d0d3f43c8680579f2b4cb
Vlad Cacenschi <cacenschivlad@gmail.com>
2024-07-29 11:18:08 +0300

    login view

--- ccc/views.py
+++ ccc/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    
+    return render(request, 'login.html')
+
+def landing_view(request):
+    return HttpResponse('Hello World')
