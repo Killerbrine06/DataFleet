671c5962a3fd27c9f3c5d0dcf4aa4b43ba6a3949
Vlad Cacenschi <cacenschivlad@gmail.com>
2024-07-31 12:22:39 +0300

    new inspection views

--- inspection/urls.py
+++ inspection/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import new_ccos_view
+from .views import *
 
 urlpatterns = [
-    path('add/', new_ccos_view, name='new_ccos_page')
+    path('add/', new_ccos_view, name='new_ccos_page'),
+    path('<int:id>', ccos_view, name='ccos_page'),
+    path('', ccos_list_view, name='ccos_list_page'),
 ]
--- inspection/views.py
+++ inspection/views.py
@@ -1,10 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.exceptions import PermissionDenied
 from .forms import CCOSCreationForm
+from .models import CCOS
 
 def new_ccos_view(request):
     if not request.user.is_authenticated:
         raise PermissionDenied
     
-    return render(request, 'new_ccos.html', {'form': CCOSCreationForm()})
+    if request.method == 'POST':
+        form = CCOSCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('index_page')
+        
+        return render(request, 'new_ccos.html', {'form': form})
+    
+    form = CCOSCreationForm()
+    
+    return render(request, 'new_ccos.html', {'form': form})
+
+def ccos_list_view(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied
+    
+    inspections = CCOS.objects.all()
+    
+    return render(request, 'ccos_list.html', {'inspections': inspections})
+
+def ccos_view(request, id:int):
+    if not request.user.is_authenticated:
+        raise PermissionDenied
+    
+    inspection = get_object_or_404(CCOS, id=id)
+    return HttpResponse(inspection.project.name)
